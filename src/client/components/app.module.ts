a15d6a1d0ebf5eda47311dc19694061be83c2f29
chen0 <aitudouing@gmail.com>
2017-09-30 00:31:55 -0400

    Here is the backend for joinging a lobby (#40)
    Magic Happens!

--- src/client/components/app.module.ts
+++ src/client/components/app.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { ParticlesModule } from 'angular-particle';
+import { AlertCenterModule  } from 'ng2-alert-center';
+
+import { AvatarModule } from 'ngx-avatar'; 
 import { ModalModule } from 'ngx-bootstrap/modal';
+
 import { Game } from '../services/game';
 import { GameSessionService } from '../services/game.session.service';
 import { AppRoutingModule, routingComponents } from './app.routing';
@@ -16,7 +22,10 @@ import { MainComponent } from './main/main.component';
         AppRoutingModule, 
         HttpModule, 
         JsonpModule, 
-        ParticlesModule
+        ParticlesModule, 
+        AlertCenterModule, 
+        BrowserAnimationsModule, 
+        AvatarModule
     ],
     declarations: [
         MainComponent,
